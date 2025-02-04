28b91275d9991fcea460bef1a90c6f13db0e960d
N Magee <nem2p@virginia.edu>
2025-02-04 15:32:34 -0500

    Full load

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,3 +1,5 @@
+// try on https://play.rust-lang.org/
+
 use rand::Rng;
 use std::io::{self, Write};
 
@@ -17,15 +19,25 @@ fn main() {
             .expect("Failed to read line");
             
         let guess: i32 = match guess.trim().parse() {
-
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
         };
         
         if guess < 1 || guess > 10 {
-
+            println!("Please guess a number between 1 and 10!");
+            continue;
         }
         
         match guess.cmp(&secret_number) {
-
+            std::cmp::Ordering::Less => println!("Too low!"),
+            std::cmp::Ordering::Greater => println!("Too high!"),
+            std::cmp::Ordering::Equal => {
+                println!("You got it! The number was {}!", secret_number);
+                break;
+            }
         }
     }
 }
